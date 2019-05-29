e9dd34b7368fe2b784b6bd7910bfadc97dcc7226
chasestarr <starr@uber.com>
2019-05-29 13:42:45 -0700

    adds mockups and color scale

--- src/lessons/customizing-themes/index.js
+++ src/lessons/customizing-themes/index.js
@@ -1,6 +1,16 @@
 import React from "react";
 import {Block} from "baseui/block";
 
+// purple color scale:
+// "#F0EEFE"
+// "#D9D4FC"
+// "#ACA0F9"
+// "#705BF4"
+// "#4327F1"
+// "#331EB7"
+// "#281791"
+// "#1B1060"
+
 function CustomizingThemes() {
   return (
     <Block display="flex" justifyContent="center" font="font500">
